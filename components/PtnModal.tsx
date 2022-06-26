39a419e0326a88b30acd9f55d7f1868ec9fc9fbb
Scott Block <scottrblock@gmail.com>
2022-06-26 02:03:56 -0400

    Update PtnModal.tsx
    cleanup state management of modal

--- components/PtnModal.tsx
+++ components/PtnModal.tsx
@@ -4,33 +4,30 @@ import { Button, Card, Modal } from "preact-bulma";
 
 import "bulma/css/bulma.min.css";
 
+const LOGSEQ_UI_CHANGED = "ui:visible:changed";
+type Status = "WAITING";
+
 function PtnModal() {
   console.log("ptn log --- PtnModal");
+
+  const [status, setStatus] = useState<Status>("WAITING");
 
   const [modalOpen, setModalOpen] = useState(true);
   const [numCount, setNumCount] = useState(0);
 
-  const modalClass = modalOpen ? "mdc-dialog--open" : "";
-
-  const handleClose = () => {
-    setModalOpen(false);
-    logseq.hideMainUI();
-  };
-
   useEffect(() => {
-    const eventName = "ui:visible:changed";
     const handler = async ({ visible }: any) => {
       setModalOpen(visible);
     };
-    logseq.on(eventName, handler);
+    logseq.on(LOGSEQ_UI_CHANGED, handler);
     return () => {
-      logseq.off(eventName, handler);
+      logseq.off(LOGSEQ_UI_CHANGED, handler);
     };
   }, []);
 
   return (
     <div>
-      <Modal.Card title="ptn modal" active={modalOpen} onClose={() => handleClose()}>
+      <Modal.Card title="ptn modal" active={modalOpen} onClose={() => logseq.hideMainUI()}>
         <Modal.CardBody>
           <Button outlined onClick={() => setNumCount(numCount + 1)}>
             this button has been clicked {numCount} times
